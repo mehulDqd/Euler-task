366235c6cb92821a365c97e9eb5cc4f3584927b7
Mehul <mgaidhani@deqode.com>
2021-09-24 23:35:21 +0530

    Updated dashboard removed walletNotConnected

--- src/Container/Dashboard/index.tsx
+++ src/Container/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@material-ui/core";
+import PairInformation from "../../Components/PairInformation";
 import SwapCard from "../../Components/SwapCard";
 import WalletDetailsCard from "../../Components/WalletDetailsCard";
 import { useStyles } from "./style";
@@ -8,6 +9,7 @@ const Dashboard = () => {
   return (
     <Box sx={{ margin: "100px auto" }} className={styles.walletContent}>
       <WalletDetailsCard />
+      <PairInformation />
       <SwapCard />
     </Box>
   );
