c4d2a6c1f52e4767a6a611634e759dc18a6c05f6
Mehul <mgaidhani@deqode.com>
2021-09-24 13:53:06 +0530

    Added context and required interface for it

--- src/Context/index.tsx
+++ src/Context/index.tsx
@@ -1,38 +1,78 @@
-import { useState } from "react";
-import * as React from "react";
-import onboardUser from "../Utils";
+import React, { useState } from "react";
+import onboardUser from "../Utils/connectWallet";
+import {
+  getUserETHBalance,
+  getUserTokenBalance,
+} from "../Utils/userTokenBalance";
+import { daiContractAddress, decimals } from "../constants";
+import { UserDetails } from "../Types/userDetails.interface";
+import CustomAlert from "../Components/Alert";
 
 export type ContextType = {
-  userLoggedIn: boolean;
   handleUserLogin(): void;
   handleLogout(): void;
-  userDetails: object;
+  userDetails: UserDetails;
+  getUserBalance(address: string): void;
 };
 
 export const AppContext = React.createContext<ContextType | null>(null);
 
 const AppProvider: React.FC<React.ReactNode> = ({ children }) => {
   const onboard = onboardUser();
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
-  const [userDetails, setUserDetails] = useState({});
+  const [userDetails, setUserDetails] = useState<UserDetails>({
+    address: "",
+    appNetworkId: 0,
+    balance: "",
+    daiBalance: "",
+    ethBalance: 0,
+    mobileDevice: false,
+    network: 0,
+    wallet: {},
+  });
+
+  const getUserBalance = async (address: string) => {
+    const ethBalance = await getUserETHBalance();
+    const daiBalance = await getUserTokenBalance(
+      address,
+      daiContractAddress,
+      decimals
+    );
+    setUserDetails({ ...userDetails, ethBalance, daiBalance });
+  };
+
   const handleUserLogin = async () => {
-    await onboard.walletSelect();
-    if (!await onboard.getState().address) {
-      await onboard.walletCheck();
+    if (onboard) {
+      await onboard.walletReset();
+      await onboard.walletSelect();
+      if (!(await onboard.getState().address)) {
+        await onboard.walletCheck();
+      }
       const details = await onboard.getState();
       setUserDetails(details);
+      await getUserBalance(details.address);
     }
-    const details = await onboard.getState();
-    setUserDetails(details);
   };
 
   const handleLogout = async () => {
-    await onboard.walletReset();
-    setUserDetails({});
+    if (onboard) {
+      await onboard.walletReset();
+      setUserDetails({
+        address: "",
+        appNetworkId: 0,
+        balance: "",
+        daiBalance: "",
+        ethBalance: 0,
+        mobileDevice: false,
+        network: 0,
+        wallet: {},
+      });
+    }
   };
 
   return (
-    <AppContext.Provider value={{ userLoggedIn, handleUserLogin, userDetails, handleLogout }}>
+    <AppContext.Provider
+      value={{ handleUserLogin, userDetails, handleLogout, getUserBalance }}
+    >
       {children}
     </AppContext.Provider>
   );
---
+++ src/Types/userDetails.interface.ts
@@ -0,0 +1,10 @@
+export interface UserDetails {
+  address: string;
+  appNetworkId: number;
+  balance: string;
+  daiBalance?: string;
+  ethBalance?: number;
+  mobileDevice: boolean;
+  network: number;
+  wallet: object;
+}
