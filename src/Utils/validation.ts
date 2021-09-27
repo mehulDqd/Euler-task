3c3e1971ec9917a1b92277aff9f807e37f4184e2
Mehul <mgaidhani@deqode.com>
2021-09-27 18:13:23 +0530

    Updated utility function

--- src/Utils/validation.ts
+++ src/Utils/validation.ts
@@ -1,17 +1,23 @@
-import { ethers } from "ethers";
+import { ethers, BigNumber } from "ethers";
 
 export const handleValidation = (
   amount: string,
   balance: string | undefined
 ) => {
   let errorsMessage: string = "";
+  const tokenAmount = BigNumber.from(
+    ethers.utils.parseEther(amount.toString() || "0")
+  );
+  const userBalance = BigNumber.from(
+    ethers.utils.parseEther(balance?.toString() || "0")
+  );
   if (!amount) {
     errorsMessage = "Cannot Be Empty";
   }
   if (!(+amount > 0)) {
     errorsMessage = "Value should be positive";
   }
-  if (balance && +amount > 0 && +balance < +amount.toString()) {
+  if (userBalance && +amount > 0 && userBalance.lt(tokenAmount)) {
     errorsMessage = "Insufficient Balance";
   }
 
